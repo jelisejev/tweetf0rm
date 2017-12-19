fe8d68f7d8ac383aa1eff5c649089ce8d4ef0dbb
jelisejev <687830+jelisejev@users.noreply.github.com>
2017-12-20 01:55:42 +0200

    batch insert

--- tweetf0rm/handler/mongodb_handler.py
+++ tweetf0rm/handler/mongodb_handler.py
@@ -19,32 +19,28 @@ def flush_row(db, bucket, items):
 	try:
 		col = db[bucket]
 
-		# bucket_folder = os.path.abspath('%s/%s'%(output_folder, bucket))
-        #
 		for k, lines in items.iteritems():
-			entry = {
-				"key": k,
-				"values": map(lambda line: json.loads(line), lines)
-			}
-			col.insert_one(entry)
-
-		# 	filename = os.path.abspath('%s/%s'%(bucket_folder, k))
-		# 	with open(filename, 'ab+') as f:
-		# 		for line in lines:
-		# 			f.write('%s\n'%line)
-        #
-		# 	logger.debug("flushed %d lines to %s"%(len(lines), filename))
+			parsed_lines = map(lambda line: json.loads(line), lines)
+			col.insert_many(parsed_lines)
 
 	except:
 		logger.error(full_stack())
 
 	return True
 
+FLUSH_SIZE = 100
+
 class MongoDBHandler(BaseHandler):
 
 	def __init__(self, config):
 		super(MongoDBHandler, self).__init__()
-		self.config = config;
+		self.config = config
+
+	def need_flush(self, bucket):
+		if (len(self.buffer[bucket]) >  FLUSH_SIZE):
+			return True
+		else:
+			return False
 
 
 	def flush(self, bucket):
